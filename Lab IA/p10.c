8686de8c13460e25a0e38f58bb6699bd969d386c
Srishti <255105205+srisbh@users.noreply.github.com>
2026-06-08 09:20:00 +0530

    Refactor quicksort with user input for array size
    Updated quicksort implementation to include user-defined array size and improved output formatting.

--- Lab IA/p10.c
+++ Lab IA/p10.c
@@ -1,64 +1,59 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-
 void quick(int a[],int l,int h)
 {
-    int i,j,pivot,temp;
-
+    int i,j,p,t;
     if(l<h)
     {
-        pivot=a[h];
+        p=a[h];
         i=l-1;
-
         for(j=l;j<h;j++)
-            if(a[j]<pivot)
-            {
-                i++;
-
-                temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
-            }
-
-        temp=a[i+1];
-        a[i+1]=a[h];
-        a[h]=temp;
-
-        int p=i+1;
-
-        quick(a,l,p-1);
-        quick(a,p+1,h);
+        if(a[j]<p)
+        {
+            i++;
+            t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+        }
+    t=a[i+1];
+    a[i+1]=a[h];
+    a[h]=t;
+    int k=i+1;
+    quick(a,l,k-1);
+    quick(a,k+1,h);
     }
 }
-
 int main()
 {
-    int a[6000],i,n=6000;
-
+    int a[6000],i,n;
+    print("Enter n value: ");
+    scanf("%d",&n);
     srand(time(0));
-
-    printf("Random numbers for n = %d:\n",n);
-
+    printf("\n Random number for n=%d:\n",n);
     for(i=0;i<n;i++)
     {
         a[i]=rand()%1000;
-        printf("%d ",a[i]);
+        printf("%d",a[i]);
+        if(i%20==0)
+        printf("\n");
     }
-
     clock_t start=clock();
-
     quick(a,0,n-1);
-
     clock_t end=clock();
 
-    printf("\nTime taken to sort for n = %d: %lf seconds\n",
+    printf("\n\nTime taken to sort for n = %d : %lf seconds\n",
             n,(double)(end-start)/CLOCKS_PER_SEC);
-
     printf("\nSorted numbers for n = %d:\n",n);
 
     for(i=0;i<n;i++)
+    {
         printf("%d ",a[i]);
 
+        if(i%20==0)
+            printf("\n");
+    }
+
     return 0;
+}
 }
